b5443f1b9d1994bca52d517e4519701fa0acfc4b
Steven B <steven@botzek.com>
2025-12-23 06:50:37 -0800

    Use OOP style calls on stats

--- src/rpg-stats.adb
+++ src/rpg-stats.adb
@@ -22,7 +22,7 @@ package body RPG.Stats is
 
    procedure Heal (HP : in Out Creature_HP; Amount : Creature_Current_HP) is
       New_Current : constant Natural := Natural(HP.Current) + Natural(Amount);
-      Max_Value   : constant Natural := Natural(Creature_Maximum_HP.Value(HP.Maximum));
+      Max_Value   : constant Natural := Natural(HP.Maximum.Value);
    begin
       if New_Current >= Max_Value then
          HP.Current := Creature_Current_HP(Max_Value);
@@ -33,7 +33,7 @@ package body RPG.Stats is
 
    procedure Full_Heal (HP : in Out Creature_HP) is
    begin
-      HP.Current := Creature_Current_HP(Creature_Maximum_HP.Value(HP.Maximum));
+      HP.Current := Creature_Current_HP(HP.Maximum.Value);
    end Full_Heal;
 
    procedure Increase_Maximum_Base (HP : in Out Creature_HP; Amount : Creature_Maximum_HP.Base_T) is
@@ -52,7 +52,7 @@ package body RPG.Stats is
       else
          HP.Maximum.Base := HP.Maximum.Base - Amount;
       end if;
-      Max_Value := Natural(Creature_Maximum_HP.Value(HP.Maximum));
+      Max_Value := Natural(HP.Maximum.Value);
       if Natural(HP.Current) > Max_Value then
          HP.Current := Creature_Current_HP(Max_Value);
       end if;
@@ -69,7 +69,7 @@ package body RPG.Stats is
       else
          HP.Maximum.Modifier := Creature_Maximum_HP.Modifier_T(New_Modifier);
       end if;
-      Max_Value := Natural(Creature_Maximum_HP.Value(HP.Maximum));
+      Max_Value := Natural(HP.Maximum.Value);
       if Natural(HP.Current) > Max_Value then
          HP.Current := Creature_Current_HP(Max_Value);
       end if;
@@ -77,7 +77,7 @@ package body RPG.Stats is
 
    function Defense (S : Creature_Stats) return Natural is
    begin
-      return 10 + Natural(Agility.Value(S.Agility)) / 2;
+      return 10 + Natural(S.Agility.Value) / 2;
    end Defense;
 
    function Is_Dead (HP : Creature_HP) return Boolean is
